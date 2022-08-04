3f3d8f7ece9832e79941caf1f1abc708dd3eb58d
Sophisticated-Chaos <84561580+Sophisticated-Chaos@users.noreply.github.com>
2022-08-04 22:37:40 +0530

    Add files via upload

---
+++ kwitter/kwitter_page.js
@@ -0,0 +1,44 @@
+function send(){
+    msg = document.getElementById("msg").value;
+    firebase.database().ref(room_name).push({
+        Name: user_name,
+        Message: msg,
+        Like: 0
+    }); 
+}
+function Logout(){
+    localStorage.removeItem("room_name");
+    localStorage.removeItem("user_name");
+    window.location= "kwitter.html";
+  }
+
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+    firebase_message_id = childKey;
+    message_data = childData;
+
+    console.log(firebase_message_id);
+    console.log(message_data);
+    name = message_data['name'];
+    message = message_data['message'];
+    like = message_data['like'];
+    name_with_tag = "<h4>"+ name +"<img class='verified_tick' src='tick.png'></h4>";
+    message_with_tag = "<h4 class='message_h4'>"+ message +"</h4>";
+    like_button = "<button class='btn btn-warning' id="+firebase_message_id+"value ="+like+ "onclick='updateLike(this.id)'>";
+    span_with_tag = "span class='glyphicon glyphicon-thumbs-up'> Like: "+ like +"</span></button><hr>";
+
+    row = name_with_tag + message_with_tag + like_button + span_with_tag;
+    document.getElementById("output").innerHTML += row;
+
+} });  }); }
+
+function updateLike(message_id){
+    console.log("clicked on like button - " + message_id);
+    button_id = message_id;
+    likes = document.getElementById(button_id).value;
+    updated_likes = Number(likes) + 1;
+    console.log(updated_likes);
+
+    firebase.database().ref(room_name).child(message_id).update({
+        like : updated_likes
+    })
+}
